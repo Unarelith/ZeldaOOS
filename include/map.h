6c6cb7f2a2c697350bd70f8dd24e058fcb2387a0
Quent42340 <quent42340@gmail.com>
2014-01-14 22:08:22 +0100

    Working on maps.

---
+++ include/map.h
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  map.h
+ *
+ *    Description:  Map system with scrolling
+ *
+ *        Version:  1.0
+ *        Created:  14/01/2014 22:05:59
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Quentin BAZIN, <quent42340@gmail.com>
+ *        Company:  Deloptia
+ *
+ * =====================================================================================
+ */
+#ifndef MAP_H_
+#define MAP_H_
+
+
+
+#endif // MAP_H_H
---
+++ source/map.c
@@ -0,0 +1,20 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  map.c
+ *
+ *    Description:  Map system with scrolling
+ *
+ *        Version:  1.0
+ *        Created:  14/01/2014 22:06:57
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Quentin BAZIN, <quent42340@gmail.com>
+ *        Company:  Deloptia
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <nds.h>
+
